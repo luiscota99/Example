8d7d70396566943ff6ab01983da394b880c40c24
luiscota99 <48337905+luiscota99@users.noreply.github.com>
2021-12-22 21:35:52 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Components/Home";
+import Gallery from "./Components/Gallery";
+import About from "./Components/About";
+
+function App() {
+  return (
+    <Fragment>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Main page</Link>
+            </li>
+            <li>
+              <Link to="/gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </div>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/gallery">
+            <Gallery />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      <footer> Hello I'm a little footer</footer>
+    </Fragment>
+  );
+}
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+const Home = () => {
+  const history = useHistory();
+
+  const handleAbout = () => history.push("/about"); //add programmatic routing
+  const handleBack = () => history.goBack(); //Back programmatic routing
+  const handleForward = () => history.goForward(); //Forward programmatic routing
+
+  return (
+    <div>
+      <h1>Hello, I'm Home component </h1>
+      <hr />
+      <h2>useHistory</h2>
+      <button onClick={handleAbout}>Go to "About"</button>
+      <button onClick={handleBack}>Back</button>
+      <button onClick={handleForward}>Forward</button>
+      <hr />
+    </div>
+  );
+};
+
+export default Home;
